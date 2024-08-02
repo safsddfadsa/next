f1560a15dd1ba465e91d466b841f801f7f8598c6
DESKTOP-ATANC99\77615 <776150961@qq.com>
2024-08-02 17:26:45 +0800

    路由1

---
+++ my-app/app/products/[...id]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductsId(props:any) {
+    console.log(props);
+    
+    return (
+      <h1>详情</h1>
+    );
+  }
+  
---
+++ my-app/app/products/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Products(props:any) {
+
+    return (
+      <h1>products</h1>
+    );
+  }
+  
